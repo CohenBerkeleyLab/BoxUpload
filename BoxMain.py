04e1b8ec73c9750788b9d5161d112d82d5b40160
Josh Laughner <joshlaugh5@gmail.com>
2017-03-01 11:34:03 -0800

    BoxMain set up to exit 1 if any files missing in specified remote, matching pattern

--- BoxMain.py
+++ BoxMain.py
@@ -1,3 +1,33 @@
 from __future__ import print_function
 import BoxUtils as utils
+import argparse
+import pdb
 
+def get_args():
+    parser = argparse.ArgumentParser(description="Determine whether Box has all the files it should")
+    parser.add_argument("localdir", help="The local directory to be mirrored")
+    parser.add_argument("remotedir", help="The remote directory that should equal the local one. Do not start with /")
+    parser.add_argument("--pattern", default=None, help="A regular express to match against file names")
+    parser.add_argument("--verbose","-v", action="count", help="Increase verbosity; will print missing files or indicate what files are being checked ")
+
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+    localdir = args.localdir
+    remotedir = args.remotedir
+    filepattern = args.pattern
+    utils.DEBUG_LEVEL = args.verbose
+
+    if isinstance(filepattern, str):
+        are_missing = utils.are_remote_files_missing(localdir, remotedir, filepat=filepattern)
+    else:
+        are_missing = utils.are_remote_files_missing(localdir, remotedir)
+
+    if are_missing:
+        exit(1)
+    else:
+        exit(0)
+
+if __name__ == "__main__":
+    main()
--- BoxUtils.py
+++ BoxUtils.py
@@ -3,8 +3,10 @@ import os
 import sys
 import subprocess
 import warnings
+import re
 
 box_url = "ftp.box.com"
+DEBUG_LEVEL = 0
 
 def shell_msg(msg):
     """
@@ -29,14 +31,29 @@ def remove_hidden_files(files):
         files.remove(h)
 
 
-def iter_dir_tree(top, nohidden=True):
+def iter_dir_tree(top, nohidden=True, pattern=".*"):
+    """
+    A generator that iterates over the directories within top and yields each file one-by-one
+    :param top: the directory to walk
+    :param nohidden: whether to include hidden files; default = True
+    :param pattern: a regular expression to be applied against file names (default = ".*"). Only matching files will be returned
+    :return: iteration over all files within top.
+    """
     for root, dirs, files in os.walk(top):
         if nohidden:
             remove_hidden_files(dirs)
             remove_hidden_files(files)
         for f in files:
-            yield os.path.join(root, f)
+            if re.match(pattern, f):
+                yield os.path.join(root, f)
 
+def are_remote_files_missing(localdir, remotedir, filepat=".*"):
+    missing = _find_missing_remote_files_recursive(localdir, remotedir, filepat=filepat)
+    if DEBUG_LEVEL > 0:
+        print("Summary of files missing from remote:")
+        for f in missing:
+            print("  {0}".format(f))
+    return len(missing) > 0
 
 def _remove_path_head(path, head):
     """
@@ -53,7 +70,7 @@ def _remove_path_head(path, head):
     return path
 
 
-def _find_missing_remote_files_recursive(localdir, remotedir):
+def _find_missing_remote_files_recursive(localdir, remotedir, filepat=".*"):
     # Get the listing of all files in the remote directory
     lftp_cmd = "find {0}; bye".format(remotedir)
     try:
@@ -72,10 +89,15 @@ def _find_missing_remote_files_recursive(localdir, remotedir):
         lsremote.remove('')
 
     missing_files = []
-    for flocal in iter_dir_tree(localdir):
+    for flocal in iter_dir_tree(localdir, pattern=filepat):
         flocal = _remove_path_head(flocal, localdir)
+        foundstr = "Found"
         if flocal not in lsremote:
             missing_files.append(flocal)
+            foundstr = "MISSING"
+
+        if DEBUG_LEVEL > 1:
+            print("Checking for {0} on remote... {1}".format(flocal, foundstr))
 
     return missing_files
 
